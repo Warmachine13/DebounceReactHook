b81106b2e0a11d05a4d4d82ac49fe7517b49a1c2
Lucas Pereira de Souza <36778572+Warmachine13@users.noreply.github.com>
2021-07-07 11:59:04 -0300

    Create debounceHook.tsx
    create debounce hook

---
+++ debounceHook.tsx
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react';
+export default function useDebouncedValue<T>(input: T, time = 500) {
+  const [debouncedValue, setDebouncedValue] = useState(input);
+  // every time input value has changed - set interval before it's actually commited
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(input);
+    }, time);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [input, time]);
+  return debouncedValue;
+}
